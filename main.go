6e79c8cb96107125e190ee8b68d931e23e9dbdde
Shikhar Kannoje <shikharkannoje09@gmail.com>
2020-08-15 22:35:00 +0530

    trying api's

--- main.go
+++ main.go
@@ -1,25 +1,28 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 
+	"github.com/joho/sqltocsv"
+	_ "github.com/joho/sqltocsv"
 	_ "github.com/lib/pq"
 )
 
 const (
-	host     = "localhost"
-	port     = 5432
-	user     = "postgres"
-	password = "your-password"
-	dbname   = "calhounio_demo"
+	hostname     = "localhost"
+	hostport     = 5432
+	username     = "postgres"
+	password     = "root"
+	databasename = "ConstructionPlan"
 )
 
 func startupServer() {
 
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+"password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
-
+	//psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+"password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	log.Println("The server is up")
 	http.HandleFunc("/home", home)
 	http.HandleFunc("/getMasterPlan", getMasterPlan)
 	http.HandleFunc("/getDates", getDates)
@@ -36,7 +39,34 @@ func home(w http.ResponseWriter, r *http.Request) {
 }
 
 func getMasterPlan(w http.ResponseWriter, r *http.Request) {
+	conStr := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", hostname, hostport, username, password, databasename)
 
+	db, err := sql.Open("postgres", conStr)
+	if err != nil {
+		log.Println(err)
+		return
+	} else {
+		log.Println("Database connected")
+	}
+
+	rows, err := db.Query(`SELECT * FROM public.masterplan ORDER BY string_to_array(slno, '.')::int[]`)
+	if err != nil {
+		log.Println(err)
+		w.Write([]byte("Error Fetching from DB"))
+		return
+	}
+	log.Println(rows)
+	err = sqltocsv.WriteFile("./report.csv", rows)
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	w.Header().Set("Content-type", "text/csv")
+	w.Header().Set("Content-Disposition", "attachment; filename=\"report.csv\"")
+	log.Println("done write till here")
+	sqltocsv.Write(w, rows)
+	return
 }
 
 func getDates(w http.ResponseWriter, r *http.Request) {
